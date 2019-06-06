e6a314542a693fcd468ff7daa6a75307004da8b7
lordoscarr <paroscarandersson@gmail.com>
2019-06-06 21:12:25 +0200

    Updates

---
+++ precache-manifest.e8b885e56f64030a229534cb2d4370d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e66298cd55b3e03ebed46b5bb4bf5b97",
+    "url": "/vg-assignment/index.html"
+  },
+  {
+    "revision": "473325e903a870991e62",
+    "url": "/vg-assignment/static/css/main.f1cd9cc3.chunk.css"
+  },
+  {
+    "revision": "4bf4a14292ed3ede8bdb",
+    "url": "/vg-assignment/static/js/2.b54dba6c.chunk.js"
+  },
+  {
+    "revision": "473325e903a870991e62",
+    "url": "/vg-assignment/static/js/main.377fc448.chunk.js"
+  },
+  {
+    "revision": "2c9b625d9701071d4114",
+    "url": "/vg-assignment/static/js/runtime~main.975997ed.js"
+  }
+]);
